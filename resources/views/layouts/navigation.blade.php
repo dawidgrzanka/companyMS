5a4bc6a5d132d31031030559edb8a87d967451db
Dawid Grzanka <dgrzanka6@gmail.com>
2024-12-15 21:01:48 +0100

    Zmiana nazwy systemu i dodanie logo z favicon

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('dashboard') }}" class="flex items-center">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <span class="ml-2 text-s font-semibold text-gray-800">companyMS</span>
                     </a>
                 </div>
 
